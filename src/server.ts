b9c5240073f1ebe2a17d24209b1e8cf94adeed91
riya1689 <riyaratri24@gmail.com>
2026-07-07 16:20:37 +0600

    feat: update server with socket.io

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app';
+import { connectDB } from './config/db';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  await connectDB();
+
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+  app.set('io', io);
+
+  io.on('connection', (socket) => {
+    console.log('A client connected:', socket.id);
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+    });
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+  });
+};
+
+process.on('unhandledRejection', (err: any) => {
+  console.log('UNHANDLED REJECTION!  Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+startServer();
