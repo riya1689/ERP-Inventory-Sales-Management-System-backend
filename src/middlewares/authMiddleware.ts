464baf543c085a1cf77f3e6e8284fd51fa997ffb
riya1689 <riyaratri24@gmail.com>
2026-07-07 15:54:31 +0600

    feat: implement auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { AppError } from '../utils/AppError';
+import { User, IUser } from '../modules/user/user.model';
+
+export interface AuthRequest extends Request {
+  user?: IUser;
+}
+
+export const protect = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
+
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(new AppError('The user belonging to this token does no longer exist.', 401));
+    }
+
+    req.user = currentUser;
+    next();
+  } catch (error) {
+    next(new AppError('Invalid token or token has expired. Please log in again.', 401));
+  }
+};
+
+export const authorize = (...roles: string[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role as string)) {
+      return next(new AppError('You do not have permission to perform this action', 403));
+    }
+    next();
+  };
+};
